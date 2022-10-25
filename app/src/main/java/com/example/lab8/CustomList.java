73357cc7b32d190589058924bac6ba4bb3ce543d
Tanu Goyal <tanugoyal@Tanus-MacBook-Pro.local>
2022-10-25 17:59:51 -0600

    Added deleteCity Method

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -62,7 +62,7 @@ public class CustomList extends ArrayAdapter<City> {
         return cities.contains(city);
     }
 
-//    public void deleteCity(City city){
-//        cities.remove(city);
-//    }
+    public void deleteCity(City city){
+        cities.remove(city);
+    }
 }
