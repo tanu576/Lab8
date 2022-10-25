5c95cebb3824332cd15cad78a2bf5268d52abab9
Tanu Goyal <tanugoyal@Tanus-MacBook-Pro.local>
2022-10-25 17:58:40 -0600

    Added hasCity method

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -58,9 +58,9 @@ public class CustomList extends ArrayAdapter<City> {
         cities.add(city);
     }
 
-//    public boolean hasCity(City city){
-//        return cities.contains(city);
-//    }
+    public boolean hasCity(City city){
+        return cities.contains(city);
+    }
 
 //    public void deleteCity(City city){
 //        cities.remove(city);
